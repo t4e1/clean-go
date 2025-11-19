b7fd6d66eb03b728143e554847a27a67137cd1bb
t4e1 <jtw5738@naver.com>
2025-11-19 15:37:47 +0900

    feat: inbound port interface 구현

--- clean-go/clean-b/post/internal/core/usecases/post.go
+++ clean-go/clean-b/post/internal/core/usecases/post.go
@@ -1,3 +1,34 @@
 package usecases
 
+import (
+	"github.com/t4e1/clean-go/clean-b/post/internal/core/domain"
+)
+
 // packages for business logic.
+type RESTApi struct {
+}
+
+func (*RESTApi) GetPost(postId int) (domain.Post, error) {
+
+	return domain.Post{}, nil
+}
+
+func (*RESTApi) GetPosts(query string) ([]domain.Post, error) {
+
+	return nil, nil
+}
+
+func (*RESTApi) NewPost(postInfo domain.Post) (bool, error) {
+
+	return false, nil
+}
+
+func (*RESTApi) UpdatePost(postInfo domain.Post) (bool, error) {
+
+	return false, nil
+}
+
+func (*RESTApi) DeletePost(postId int) (bool, error) {
+
+	return false, nil
+}
--- clean-go/clean-b/post/internal/ports/in/api.go
+++ clean-go/clean-b/post/internal/ports/in/api.go
@@ -1,3 +1,15 @@
 package in
 
+import (
+	"github.com/t4e1/clean-go/clean-b/post/internal/core/domain"
+)
+
 // packages for REST API(HTTP) interface
+
+type InboundRESTApiPort interface {
+	GetPost(postId int) (domain.Post, error)
+	GetPosts(query string) ([]domain.Post, error)
+	NewPost(postInfo domain.Post) (bool, error)
+	UpdatePost(postInfo domain.Post) (bool, error)
+	DeletePost(postId int) (bool, error)
+}
